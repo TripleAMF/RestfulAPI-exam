32a598567932506ebe00a63d377f5c8c5fa3a171
TripleAMF <160726028+TripleAMF@users.noreply.github.com>
2026-04-18 12:37:15 +0300

    Update DTOs.cs

--- DTOs.cs
+++ DTOs.cs
@@ -5,21 +5,21 @@ namespace MovieCatalogTests
     public class MovieDto
     {
         [JsonProperty("id")]
-        public string Id { get; set; }
+        public string? Id { get; set; }
 
         [JsonProperty("title")]
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
         [JsonProperty("description")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
     }
 
     public class ApiResponseDto
     {
         [JsonProperty("msg")]
-        public string Msg { get; set; }
+        public string? Msg { get; set; }
 
         [JsonProperty("movie")]
-        public MovieDto Movie { get; set; }
+        public MovieDto? Movie { get; set; }
     }
 }
